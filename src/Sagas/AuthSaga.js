83918f577b8b552122c1d9eb00e02dd7eb3a3c87
Brian Clarke <clarkeadg@yahoo.com>
2017-10-07 18:34:17 -0700

    signup

--- src/Sagas/AuthSaga.js
+++ src/Sagas/AuthSaga.js
@@ -130,7 +130,7 @@ export default (api) => {
     if (response && response.ok) {
       //yield put(Actions.signupSuccess())
       yield put(Actions.loginAttempt({
-        username: meta.username,
+        email: meta.email,
         password: meta.password
       }))
     } else {
